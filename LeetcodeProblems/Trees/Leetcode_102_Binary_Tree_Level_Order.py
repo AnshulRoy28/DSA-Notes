85c8ca9959a58c02c8e6d471c60f9f54b2b59979
AnshulRoy28 <151902302+AnshulRoy28@users.noreply.github.com>
2025-08-10 00:53:50 +0530

    Update Leetcode_102_Binary_Tree_Level_Order.py

--- LeetcodeProblems/Trees/Leetcode_102_Binary_Tree_Level_Order.py
+++ LeetcodeProblems/Trees/Leetcode_102_Binary_Tree_Level_Order.py
@@ -1,24 +1,28 @@
 from collections import deque
 def levelOrder(self, root):
         if not root:
-            return []
+            return [] #Tree empty return 
     
         result=[]
-        queue=deque([root])
+        queue=deque([root]) #Use Queue for BFS traversal 
 
         while queue:
-            level_size=len(queue)
-            current_level=[]
+            level_size=len(queue) #Number of nodes at current lvl 
+            current_level=[] #List to hold values of current lvl 
 
+            #Process nodes at current lvl 
             for i in range(level_size):
-                node=queue.popleft()
-                current_level.append(node.val)
+                node=queue.popleft() #Remove node from que
+                current_level.append(node.val) #Add node's value to current_lvl
 
+                #If left child exists add to queue
                 if node.left:
                     queue.append(node.left)
                 
+                #If right child exists add to queue
                 if node.right:
                     queue.append(node.right)
+            #After processing add current lvl val to result
             result.append(current_level)
         return result    
 
